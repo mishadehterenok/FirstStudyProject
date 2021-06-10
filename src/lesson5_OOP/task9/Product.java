1edf0bf47f85c5e117ac5a44b9cc0361023006f4
mishadehterenok <m.dhtrnk@gmail.com>
2021-06-10 14:06:54 +0300

    task7 + task8

---
+++ src/lesson5_OOP/task9/Product.java
@@ -0,0 +1,49 @@
+package lesson5_OOP.task9;
+
+public class Product {
+    private String name;
+    private double price;
+    private double rating;
+
+    public Product() {
+    }
+
+    public Product(String name, double price, double rating) {
+        this.name = name;
+        this.price = price;
+        this.rating = rating;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+
+    public void setRating(double rating) {
+        this.rating = rating;
+    }
+
+    public void printInfo() {
+        System.out.println("Product{" +
+                "name='" + name + '\'' +
+                ", price=" + price +
+                ", rating=" + rating +
+                '}');
+    }
+
+}
