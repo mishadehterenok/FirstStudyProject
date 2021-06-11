cb29eca0ffd2a83e22b506ce9234cdc98de71d9d
mishadehterenok <m.dhtrnk@gmail.com>
2021-06-11 22:24:36 +0300

    commit with competition task3

--- src/lesson5_6_7_OOP/competition/task3/App.java
+++ src/lesson5_6_7_OOP/competition/task3/App.java
@@ -2,8 +2,16 @@ package lesson5_6_7_OOP.competition.task3;
 
 public class App {
     public static void main(String[] args) {
-        Seasons season = Seasons.SUMMER;
+        Seasons season = Seasons.WINTER;
+        season.printInfo();
+        season.iLove(Seasons.AUTUMN);
+        Seasons.SUMMER.getDescription();
 
-
+        Seasons [] arr = Seasons.values();
+        for(Seasons i : arr){
+            System.out.println();
+            i.printInfo();
+            i.getDescription();
+        }
     }
 }
--- src/lesson5_6_7_OOP/competition/task3/Seasons.java
+++ src/lesson5_6_7_OOP/competition/task3/Seasons.java
@@ -1,16 +1,53 @@
 package lesson5_6_7_OOP.competition.task3;
 
 public enum Seasons {
-    SUMMER{
+    SUMMER("Summer","+25"){
         @Override
-        public String displayInfo(){
-            return "Warm";
+        public void getDescription (){
+            System.out.println("Warm time of the year");
         }
-    },AUTUMN,WINTER,SPRING
+    },
+    AUTUMN("Autumn","+8"),
+    WINTER("Winter","-15"),
+    SPRING("Spring","+15");
 
-    public String displayInfo(){
-        return "Seasons";
+    String season;
+    String temperature;
+
+    Seasons(String season,String temperature){
+        this.season = season;
+        this.temperature = temperature;
+    }
+    public String getSeason() {
+        return season;
+    }
+    public String getTemperature() {
+        return temperature;
     }
 
+    public void printInfo(){
+        System.out.println(season + " " + temperature);
+    }
+    public void iLove (Seasons season){
+        System.out.println("I love " + getSeason());
 
+        /*System.out.print("I love ");
+        switch (season){
+            case AUTUMN:
+                System.out.println("Autumn");
+                break;
+            case WINTER:
+                System.out.println("Winter");
+                break;
+            case SPRING:
+                System.out.println("Spring");
+                break;
+            case SUMMER:
+                System.out.println("Summer");
+                break;
+        }*/
+    }
+    public void getDescription (){
+        System.out.println("Cold time of the year");
+    }
 }
