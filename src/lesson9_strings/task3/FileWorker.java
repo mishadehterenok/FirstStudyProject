6e27421a27f494d60c3ca4439f5eed3437895f25
mishadehterenok <m.dhtrnk@gmail.com>
2021-06-21 02:08:45 +0300

    commit with some changes + task 3 about Strings

--- src/lesson9_strings/task3/FileWorker.java
+++ src/lesson9_strings/task3/FileWorker.java
@@ -3,11 +3,11 @@ package lesson9_strings.task3;
 import java.io.*;
 import java.util.Scanner;
 
-public class FileWorkerr {
+public class FileWorker {
     public static void main(String[] args) {
         final String DIRECTORY = "src" + File.separator + "lesson9_strings" + File.separator + "task3" + File.separator;
         File newFile = new File(DIRECTORY + "NewFile.txt");
-        try (Scanner scanner = new Scanner(new BufferedInputStream(new FileInputStream(DIRECTORY + "InitialFile.txt")));
+        try (Scanner scanner = new Scanner(new BufferedReader(new FileReader(DIRECTORY + "InitialFile.txt")));
              BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(newFile))) {
             while (scanner.hasNext()) {
                 String st = scanner.next();
