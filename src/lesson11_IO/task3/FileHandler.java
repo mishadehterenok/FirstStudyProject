2e4b4cefaf87b786fdc62f278b92d9bc7ae3ddbf
mishadehterenok <m.dhtrnk@gmail.com>
2021-06-23 00:13:12 +0300

    Task 3 IO Streams

--- src/lesson11_IO/task3/FileHandler.java
+++ src/lesson11_IO/task3/FileHandler.java
@@ -3,28 +3,28 @@ package lesson11_IO.task3;
 import java.io.*;
 
 public class FileHandler {
-    public static boolean CheckCensorship (String sentence){
-        boolean id = true;
-        String [] words = sentence.split("\\s?[,–:;]?\\s");
-        for(String word: words){
-            if (CompareToCensore(word)){
-                id = false;
+    public static boolean CheckCensorship(String sentence) {
+        boolean id = false;
+        String[] words = sentence.split("\\s?[,–:;]?\\s");
+        for (String word : words) {
+            if (CompareToCensore(word)) {
+                id = true;
                 break;
             }
         }
         return id;
     }
 
-    public static boolean CompareToCensore(String word){
-         final String DIRECTION = "src" + File.separator + "lesson11_IO" + File.separator + "task3" + File.separator;
+    public static boolean CompareToCensore(String word) {
+        final String DIRECTION = "src" + File.separator + "lesson11_IO" + File.separator + "task3" + File.separator;
         StringBuilder builder = new StringBuilder();
         boolean id = false;
-        try(BufferedReader reader = new BufferedReader(new FileReader(DIRECTION + "BlackList.txt"))) {
-            while (reader.ready()){
+        try (BufferedReader reader = new BufferedReader(new FileReader(DIRECTION + "BlackList.txt"))) {
+            while (reader.ready()) {
                 builder.append(reader.readLine()).append(" ");
             }
-            String [] censoreWords = builder.toString().split("\\s");
-            for(String censoreWord: censoreWords){
+            String[] censoreWords = builder.toString().split("\\s");
+            for (String censoreWord : censoreWords) {
                 if (word.equalsIgnoreCase(censoreWord)) {
                     id = true;
                     break;
@@ -36,10 +36,10 @@ public class FileHandler {
         return id;
     }
 
-    public static String [] ReadFile(String filePath){
+    public static String[] ReadFile(String filePath) {
         StringBuilder builder = new StringBuilder();
-        try(BufferedReader reader = new BufferedReader(new FileReader(filePath))){
-            while (reader.ready()){
+        try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+            while (reader.ready()) {
                 builder.append(reader.readLine()).append(" ");
             }
         } catch (IOException e) {
@@ -48,13 +48,18 @@ public class FileHandler {
         return builder.toString().split("\s?[.!?…]\s*");
     }
 
-    public static void WriteVerifiedSentencesInNewFile(String filePath, String [] sentences){
-        try(BufferedWriter writer = new BufferedWriter(new FileWriter(filePath))) {
-            for(String sentence: sentences){
-                if(CheckCensorship(sentence)){
+    public static void WriteVerifiedSentencesInNewFile(String filePath, String[] sentences) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(filePath))) {
+            int counter = 0;
+            for (String sentence : sentences) {
+                if (!CheckCensorship(sentence)) {
                     writer.write(sentence + "\n");
+                } else {
+                    counter++;
+                    System.out.println(counter + ") " +sentence);
                 }
             }
+            System.out.printf("\nOnly %d sentences failed the censorship test.\n", counter);
         } catch (IOException e) {
             e.printStackTrace();
         }
