df88445a6bd774310793c9f2b6f3ce611087a4bf
mishadehterenok <m.dhtrnk@gmail.com>
2021-07-16 17:46:39 +0300

    regular update

--- src/lesson15_regular_expressions/task/IP4_regex.java
+++ src/lesson15_regular_expressions/task/IP4_regex.java
@@ -8,6 +8,15 @@ public class IP4_regex {
         Pattern pattern = Pattern.compile("^(((00[1-9]|0[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\\.){3}(0[1-9]|1[0-6]))$");
         Matcher matcher = pattern.matcher("243.005.196.01");
         System.out.println(matcher.find());
+
+        Pattern pattern1 = Pattern.compile("\\d{3,}");
+        Matcher matcher1 = pattern1.matcher("ekn 1234 fd 1223 dcvg 12 fv 12345");
+        StringBuilder builder = new StringBuilder("");
+        while (matcher1.find()){
+            matcher1.appendReplacement(builder, String.valueOf(Integer.parseInt(matcher1.group())*2));
+        }
+        matcher1.appendTail(builder);
+        System.out.println(builder);
     }
 
 }
--- src/lesson15_regular_expressions/task/Test.java
+++ src/lesson15_regular_expressions/task/Test.java
@@ -3,25 +3,6 @@ package lesson15_regular_expressions.task;
 
 public class Test {
     public static void main(String[] args) {
-        Integer s = 4;
-        if (s.toString() == s.toString()) {
-            System.out.println("true");
-        } else System.out.println("false");
-
-        Integer i1 = 10;
-        Integer i2 = 10;
-
-        Double d1 = 10d;
-        Double d2 = 10d;
-
-        System.out.println(i1 == i2);
-        System.out.println(d1 == d2);
-
-        Integer ii = 1000;
-        Integer jj = 2000;
-
-        System.out.println((ii * 2 == jj) ? "yes" : "no");
-        System.out.println((jj / 2 == ii) ? "yes" : "no");
 
     }
 }
